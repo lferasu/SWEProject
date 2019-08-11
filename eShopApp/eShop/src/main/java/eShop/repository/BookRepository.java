f8c4f1e6471fa1f4c97287e8dd6de52d1b304cb5
Addisu <ammarilign@mum.edu>
2019-08-11 17:35:38 -0500

    Book search

--- eShopApp/eShop/src/main/java/eShop/repository/BookRepository.java
+++ eShopApp/eShop/src/main/java/eShop/repository/BookRepository.java
@@ -9,11 +9,5 @@ import eShop.model.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer>{
-<<<<<<< HEAD
 	public List<Book> findAllByIsbnContainsOrTitleContainsOrAuthorsContainsOrCategoriesContainsOrSupplierContainsOrderByTitle(String isbn, String title, String author, String category, String supplier);
-=======
-	public List<Book> findBookByIsbn(String isbn);
-	public List<Book> findAllByTitleContainsOrDescriptionContainsOrIsbnContains(String title, String desc, String isbn);
-
->>>>>>> b5428b0aaceb565e0964e1283c4dfdcd997726c3
 }
--- eShopApp/eShop/src/main/java/eShop/service/impl/BookServiceImpl.java
+++ eShopApp/eShop/src/main/java/eShop/service/impl/BookServiceImpl.java
@@ -14,80 +14,40 @@ import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
-
     @Autowired
-<<<<<<< HEAD
-    private BookRepository repository;
-
-	@Override
-	public Iterable<Book> getAllBooks() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Page<Book> getAllBooksPaged(int pageNo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Book saveBook(Book book) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Book getBookById(Integer bookId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deleteBookById(Integer bookId) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<Book> getAllBooksQuickSearch(String searchString) {
-return repository.findAllByIsbnContainsOrTitleContainsOrAuthorsContainsOrCategoriesContainsOrSupplierContainsOrderByTitle(searchString,searchString,searchString,searchString,searchString);
-	}
-=======
     private BookRepository bookRepository;
-
     @Override
     public Iterable<Book> getAllBooks() {
-        return bookRepository.findAll(Sort.by("title"));
+        return null;
     }
-
     @Override
     public Page<Book> getAllBooksPaged(int pageNo) {
-        return bookRepository.findAll(PageRequest.of(pageNo, 5, Sort.by("title")));
+        return null;
     }
 
     @Override
     public Book saveBook(Book book) {
-        return bookRepository.save(book);
+        return null;
     }
 
     @Override
     public Book getBookById(Integer bookId) {
-        return bookRepository.getOne(bookId);
+        return null;
     }
 
     @Override
     public void deleteBookById(Integer bookId) {
-        bookRepository.deleteById(bookId);
+
     }
 
     @Override
-    public Page<Book> getQuickSearchBooks(int pageNo, String searchText) {
-        List<Book> foundBooks = bookRepository.findAllByTitleContainsOrDescriptionContainsOrIsbnContains(searchText,searchText,searchText);
-        return new PageImpl<Book>(foundBooks,PageRequest.of(pageNo, 4, Sort.by("title")), foundBooks.size());
+    public List<Book> getAllBooksQuickSearch(String searchString) {
+        return bookRepository.findAllByIsbnContainsOrTitleContainsOrAuthorsContainsOrCategoriesContainsOrSupplierContainsOrderByTitle(searchString,
+                searchString,searchString,searchString,searchString);
     }
 
->>>>>>> b5428b0aaceb565e0964e1283c4dfdcd997726c3
+
+
 
 //	@Override
 //	public Page<Book> getSearchedBooks(int pageNo, Book book) {
