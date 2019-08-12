5d8b2cee4c0813567999fc97540ec79404479408
Addisu <ammarilign@mum.edu>
2019-08-11 21:05:24 -0500

    search update

--- eShopApp/eShop/src/main/java/eShop/controller/BookController.java
+++ eShopApp/eShop/src/main/java/eShop/controller/BookController.java
@@ -1,14 +1,31 @@
 package eShop.controller;
 
+import eShop.repository.BookRepository;
+import eShop.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookService bookService;
+
     @GetMapping(value = {"book/"})
     public String home() {
         return "book/index";
+    }
+    // SEARCH Use-Case
+    @GetMapping(value = { "eshop/Book/search" })
+    public ModelAndView searchStudent(@RequestParam String search, Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("book", bookService.getAllBooksQuickSearch(search));
+        modelAndView.setViewName("Book/search");
+        return modelAndView;
     }
 
 }
