09f442b773feec26bb07339820e5182dc354bb7f
Addisu <ammarilign@mum.edu>
2019-08-12 23:39:59 -0500

    add to cart change

--- eShopApp/eShop/src/main/java/eShop/controller/CartController.java
+++ eShopApp/eShop/src/main/java/eShop/controller/CartController.java
@@ -1,34 +1,54 @@
 package eShop.controller;
 
+import eShop.model.Book;
+import eShop.model.Cart;
+import eShop.model.user.Customer;
+import eShop.model.user.User;
 import eShop.service.CartService;
+import eShop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CartController {
     @Autowired
-    private CartService cartService;
-    @GetMapping(value = {"/", "/eshop", "/eshop/cart"})
-    public String showAddtoCartPage(){
-       return "cart/addToCart";
-    }
-
-//    @PostMapping(value={"/addToCart"})
-
-
-
-
-//    // ADD-NEW Use-Case
-//    @PostMapping(value = { "/emarket/product/save" })
-//    public String addNewProduct(@Valid @ModelAttribute("student") Product product, BindingResult bindingResult) {
-//        if (bindingResult.hasErrors()) {
-//            return "/eRegistrar/product/edit";
-//        }
-//        product = productService.save(product);
-//        return "redirect:/emarket/product/list";
+    private UserService userService;
+//    @GetMapping(value = {"/", "/eshop", "/eshop/cart"})
+//    public String showAddtoCartPage(){
+//       return "cart/addToCart";
 //    }
 
+    @PostMapping(value={"/addToCart"})
+    public ModelAndView addBookToCart(@ModelAttribute("book") Book book,  BindingResult bindingResult, Model model){
 
+Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+String userName;
+
+if(principal instanceof UserDetails){
+    userName=((UserDetails)principal).getUsername();
+}
+else{
+    userName=principal.toString();
+}
+        User myUser=userService.findByUsername(userName);
+        Cart cart = new Cart();
+        Customer customer=(Customer)myUser;
+        cart.setCustomer(customer);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("cart",cart);
+        modelAndView.setViewName("book/placeorder");
+        return modelAndView;
+    }
 }
--- eShopApp/eShop/src/main/java/eShop/service/CartService.java
+++ eShopApp/eShop/src/main/java/eShop/service/CartService.java
@@ -5,10 +5,10 @@ import eShop.model.Book;
 
 import java.util.List;
 public interface CartService {
-    public abstract  void initializeCart(String person);
-    public abstract void addBookIntoCart(String bookId);
-    public abstract void removeBookFromCart(String bookId);
-    public abstract Book findById(String BookId);
-    //public List<String> getCartContent();
-    public void clearCart();
+//    public abstract  void initializeCart(String person);
+//    public abstract void addBookIntoCart(String bookId);
+//    public abstract void removeBookFromCart(String bookId);
+//    public abstract Book findBookById(Integer bookId);
+//    //public List<String> getCartContent();
+//    public void clearCart();
 }
